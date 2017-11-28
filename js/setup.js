2eb51c57bf4d0bb7339a4477d3faecdf71afb5fe
Tabriz Movsumlu <movsumlutab@gmail.com>
2017-11-28 09:28:36 +0300

    #9 Учебный проект: нас орда

--- js/setup.js
+++ js/setup.js
@@ -5,9 +5,9 @@ userDialog.classList.remove('hidden');
 
 var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 
-var lastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго','Ирвинг'];
+var lastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 
-var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)','rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 
 var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
 
@@ -25,29 +25,29 @@ function getRandomEyesColor() {
 };
 
 function createWizards() {
-   return {
-       name: getRandomNames() + ' ' + getRandomLastNames(),
-       coatColor:  getRandomCoatColor(),
-       eyesColor:  getRandomEyesColor()
+  return {
+  name: getRandomNames() + ' ' + getRandomLastNames(),
+    coatColor:  getRandomCoatColor(),
+    eyesColor:  getRandomEyesColor()
    }
 };
 
 var wizards = [];
 for (var j = 1; j <= 4 ; j++){
-     wizards.push(createWizards());
+  wizards.push(createWizards());
 };
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 for (var i=0; i < wizards.length ; i++){
   var cloneWizard = similarWizardTemplate.cloneNode(true);
-     
+
   cloneWizard.querySelector('.setup-similar-label').innerHTML = wizards[i].name;
-    
+
   cloneWizard.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-    
+
   cloneWizard.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
-    
+
   document.querySelector('.setup-similar-list').appendChild(cloneWizard);
 }
 
