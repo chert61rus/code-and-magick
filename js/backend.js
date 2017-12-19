21eb00ed08674c26e5914949837d2a44a9e64ae1
Tabriz Movsumlu <movsumlutab@gmail.com>
2017-12-19 22:38:34 +0300

    #21 Учебный проект: pimp my mage

---
+++ js/backend.js
@@ -0,0 +1,29 @@
+'use strict';
+
+(function(){
+  window.backend = {
+    load: function(onLoad, onError){
+     var xhr = new XMLHttpRequest();
+     xhr.open('GET', 'https://1510.dump.academy/code-and-magick/data');
+     xhr.addEventListener('load', function(){
+       if(xhr.status == 404){
+         onError('Произошла ошибка соединения');
+       } else {
+         var wizardsData = JSON.parse(xhr.response);
+         onLoad(wizardsData);
+       }
+     })
+     xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+     });
+     xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+     });
+     xhr.timeout = 10000; // 10s
+     xhr.send();
+    },
+    save: function(){     
+    }
+  }
+
+}())
